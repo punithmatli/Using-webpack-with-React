4522439002d6a9559b7a24f5d46b7cd693e73dec
unknown <punithmatli@gmail.com>
2020-10-09 07:44:17 +0530

    First Commit

---
+++ src/containers/Users.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+import PizzaImage from '../components/PizzaImage/PizzaImage'
+
+class Users extends Component {
+    render() {
+        return (
+            <div>
+                <h1>The Users</h1>
+                <p> Hello everyone! what's up..</p>
+            </div>
+        )
+    }
+}
+
+export default Users;
